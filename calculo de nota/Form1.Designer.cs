e405213a4f0c3eb384a90ef48286d67636e0842f
migueluapa <miguelangelluciano77@gmail.com>
2018-01-19 22:33:58 -0400

    actuazado

--- calculo de nota/Form1.Designer.cs
+++ calculo de nota/Form1.Designer.cs
@@ -28,12 +28,308 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.txtresultado = new System.Windows.Forms.TextBox();
+            this.lblresultado = new System.Windows.Forms.Label();
+            this.btncalcular = new System.Windows.Forms.Button();
+            this.lbl255 = new System.Windows.Forms.Label();
+            this.lbl25 = new System.Windows.Forms.Label();
+            this.lbl50 = new System.Windows.Forms.Label();
+            this.txtnota = new System.Windows.Forms.TextBox();
+            this.txttrabajo = new System.Windows.Forms.TextBox();
+            this.txttareas = new System.Windows.Forms.TextBox();
+            this.txtexamen = new System.Windows.Forms.TextBox();
+            this.lblnota = new System.Windows.Forms.Label();
+            this.lbltrabajo = new System.Windows.Forms.Label();
+            this.lbltareas = new System.Windows.Forms.Label();
+            this.lblexamen = new System.Windows.Forms.Label();
+            this.btnlimpiar = new System.Windows.Forms.Button();
+            this.btnespanol = new System.Windows.Forms.Button();
+            this.btningles = new System.Windows.Forms.Button();
+            this.txttelefono = new System.Windows.Forms.TextBox();
+            this.txtapellido = new System.Windows.Forms.TextBox();
+            this.txtnombre = new System.Windows.Forms.TextBox();
+            this.lbltelefono = new System.Windows.Forms.Label();
+            this.lblapellido = new System.Windows.Forms.Label();
+            this.lblnombre = new System.Windows.Forms.Label();
+            this.txtmatricula = new System.Windows.Forms.TextBox();
+            this.lblmatricula = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // txtresultado
+            // 
+            this.txtresultado.Location = new System.Drawing.Point(176, 347);
+            this.txtresultado.Name = "txtresultado";
+            this.txtresultado.Size = new System.Drawing.Size(99, 20);
+            this.txtresultado.TabIndex = 50;
+            // 
+            // lblresultado
+            // 
+            this.lblresultado.AutoSize = true;
+            this.lblresultado.Location = new System.Drawing.Point(120, 354);
+            this.lblresultado.Name = "lblresultado";
+            this.lblresultado.Size = new System.Drawing.Size(50, 13);
+            this.lblresultado.TabIndex = 49;
+            this.lblresultado.Text = "resultado";
+            // 
+            // btncalcular
+            // 
+            this.btncalcular.Location = new System.Drawing.Point(176, 387);
+            this.btncalcular.Name = "btncalcular";
+            this.btncalcular.Size = new System.Drawing.Size(75, 23);
+            this.btncalcular.TabIndex = 48;
+            this.btncalcular.Text = "calcular";
+            this.btncalcular.UseVisualStyleBackColor = true;
+            this.btncalcular.Click += new System.EventHandler(this.btncalcular_Click);
+            // 
+            // lbl255
+            // 
+            this.lbl255.AutoSize = true;
+            this.lbl255.Location = new System.Drawing.Point(254, 283);
+            this.lbl255.Name = "lbl255";
+            this.lbl255.Size = new System.Drawing.Size(27, 13);
+            this.lbl255.TabIndex = 47;
+            this.lbl255.Text = "25%";
+            // 
+            // lbl25
+            // 
+            this.lbl25.AutoSize = true;
+            this.lbl25.Location = new System.Drawing.Point(254, 255);
+            this.lbl25.Name = "lbl25";
+            this.lbl25.Size = new System.Drawing.Size(27, 13);
+            this.lbl25.TabIndex = 46;
+            this.lbl25.Text = "25%";
+            // 
+            // lbl50
+            // 
+            this.lbl50.AutoSize = true;
+            this.lbl50.Location = new System.Drawing.Point(254, 217);
+            this.lbl50.Name = "lbl50";
+            this.lbl50.Size = new System.Drawing.Size(27, 13);
+            this.lbl50.TabIndex = 45;
+            this.lbl50.Text = "50%";
+            // 
+            // txtnota
+            // 
+            this.txtnota.Location = new System.Drawing.Point(176, 316);
+            this.txtnota.Name = "txtnota";
+            this.txtnota.Size = new System.Drawing.Size(60, 20);
+            this.txtnota.TabIndex = 44;
+            // 
+            // txttrabajo
+            // 
+            this.txttrabajo.Location = new System.Drawing.Point(176, 276);
+            this.txttrabajo.Name = "txttrabajo";
+            this.txttrabajo.Size = new System.Drawing.Size(60, 20);
+            this.txttrabajo.TabIndex = 43;
+            // 
+            // txttareas
+            // 
+            this.txttareas.Location = new System.Drawing.Point(176, 248);
+            this.txttareas.Name = "txttareas";
+            this.txttareas.Size = new System.Drawing.Size(60, 20);
+            this.txttareas.TabIndex = 42;
+            // 
+            // txtexamen
+            // 
+            this.txtexamen.Location = new System.Drawing.Point(176, 210);
+            this.txtexamen.Name = "txtexamen";
+            this.txtexamen.Size = new System.Drawing.Size(60, 20);
+            this.txtexamen.TabIndex = 41;
+            // 
+            // lblnota
+            // 
+            this.lblnota.AutoSize = true;
+            this.lblnota.Location = new System.Drawing.Point(126, 319);
+            this.lblnota.Name = "lblnota";
+            this.lblnota.Size = new System.Drawing.Size(28, 13);
+            this.lblnota.TabIndex = 40;
+            this.lblnota.Text = "nota";
+            // 
+            // lbltrabajo
+            // 
+            this.lbltrabajo.AutoSize = true;
+            this.lbltrabajo.Location = new System.Drawing.Point(126, 283);
+            this.lbltrabajo.Name = "lbltrabajo";
+            this.lbltrabajo.Size = new System.Drawing.Size(39, 13);
+            this.lbltrabajo.TabIndex = 39;
+            this.lbltrabajo.Text = "trabajo";
+            // 
+            // lbltareas
+            // 
+            this.lbltareas.AutoSize = true;
+            this.lbltareas.Location = new System.Drawing.Point(126, 248);
+            this.lbltareas.Name = "lbltareas";
+            this.lbltareas.Size = new System.Drawing.Size(36, 13);
+            this.lbltareas.TabIndex = 38;
+            this.lbltareas.Text = "tareas";
+            // 
+            // lblexamen
+            // 
+            this.lblexamen.AutoSize = true;
+            this.lblexamen.Location = new System.Drawing.Point(126, 210);
+            this.lblexamen.Name = "lblexamen";
+            this.lblexamen.Size = new System.Drawing.Size(44, 13);
+            this.lblexamen.TabIndex = 37;
+            this.lblexamen.Text = "examen";
+            // 
+            // btnlimpiar
+            // 
+            this.btnlimpiar.Location = new System.Drawing.Point(237, 436);
+            this.btnlimpiar.Name = "btnlimpiar";
+            this.btnlimpiar.Size = new System.Drawing.Size(75, 23);
+            this.btnlimpiar.TabIndex = 36;
+            this.btnlimpiar.Text = "Limpiar";
+            this.btnlimpiar.UseVisualStyleBackColor = true;
+            this.btnlimpiar.Click += new System.EventHandler(this.btnlimpiar_Click);
+            // 
+            // btnespanol
+            // 
+            this.btnespanol.Location = new System.Drawing.Point(142, 433);
+            this.btnespanol.Name = "btnespanol";
+            this.btnespanol.Size = new System.Drawing.Size(75, 23);
+            this.btnespanol.TabIndex = 35;
+            this.btnespanol.Text = "Español";
+            this.btnespanol.UseVisualStyleBackColor = true;
+            this.btnespanol.Click += new System.EventHandler(this.btnespanol_Click);
+            // 
+            // btningles
+            // 
+            this.btningles.Location = new System.Drawing.Point(37, 433);
+            this.btningles.Name = "btningles";
+            this.btningles.Size = new System.Drawing.Size(75, 23);
+            this.btningles.TabIndex = 34;
+            this.btningles.Text = "Ingles";
+            this.btningles.UseVisualStyleBackColor = true;
+            this.btningles.Click += new System.EventHandler(this.btningles_Click);
+            // 
+            // txttelefono
+            // 
+            this.txttelefono.Location = new System.Drawing.Point(145, 138);
+            this.txttelefono.Name = "txttelefono";
+            this.txttelefono.Size = new System.Drawing.Size(100, 20);
+            this.txttelefono.TabIndex = 33;
+            // 
+            // txtapellido
+            // 
+            this.txtapellido.Location = new System.Drawing.Point(145, 92);
+            this.txtapellido.Name = "txtapellido";
+            this.txtapellido.Size = new System.Drawing.Size(100, 20);
+            this.txtapellido.TabIndex = 32;
+            // 
+            // txtnombre
+            // 
+            this.txtnombre.Location = new System.Drawing.Point(145, 51);
+            this.txtnombre.Name = "txtnombre";
+            this.txtnombre.Size = new System.Drawing.Size(100, 20);
+            this.txtnombre.TabIndex = 31;
+            // 
+            // lbltelefono
+            // 
+            this.lbltelefono.AutoSize = true;
+            this.lbltelefono.Location = new System.Drawing.Point(69, 138);
+            this.lbltelefono.Name = "lbltelefono";
+            this.lbltelefono.Size = new System.Drawing.Size(45, 13);
+            this.lbltelefono.TabIndex = 30;
+            this.lbltelefono.Text = "telefono";
+            // 
+            // lblapellido
+            // 
+            this.lblapellido.AutoSize = true;
+            this.lblapellido.Location = new System.Drawing.Point(68, 92);
+            this.lblapellido.Name = "lblapellido";
+            this.lblapellido.Size = new System.Drawing.Size(43, 13);
+            this.lblapellido.TabIndex = 29;
+            this.lblapellido.Text = "apellido";
+            // 
+            // lblnombre
+            // 
+            this.lblnombre.AutoSize = true;
+            this.lblnombre.Location = new System.Drawing.Point(69, 54);
+            this.lblnombre.Name = "lblnombre";
+            this.lblnombre.Size = new System.Drawing.Size(42, 13);
+            this.lblnombre.TabIndex = 28;
+            this.lblnombre.Text = "nombre";
+            // 
+            // txtmatricula
+            // 
+            this.txtmatricula.Location = new System.Drawing.Point(145, 16);
+            this.txtmatricula.Name = "txtmatricula";
+            this.txtmatricula.Size = new System.Drawing.Size(100, 20);
+            this.txtmatricula.TabIndex = 27;
+            // 
+            // lblmatricula
+            // 
+            this.lblmatricula.AutoSize = true;
+            this.lblmatricula.Location = new System.Drawing.Point(70, 16);
+            this.lblmatricula.Name = "lblmatricula";
+            this.lblmatricula.Size = new System.Drawing.Size(49, 13);
+            this.lblmatricula.TabIndex = 26;
+            this.lblmatricula.Text = "matricula";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(348, 475);
+            this.Controls.Add(this.txtresultado);
+            this.Controls.Add(this.lblresultado);
+            this.Controls.Add(this.btncalcular);
+            this.Controls.Add(this.lbl255);
+            this.Controls.Add(this.lbl25);
+            this.Controls.Add(this.lbl50);
+            this.Controls.Add(this.txtnota);
+            this.Controls.Add(this.txttrabajo);
+            this.Controls.Add(this.txttareas);
+            this.Controls.Add(this.txtexamen);
+            this.Controls.Add(this.lblnota);
+            this.Controls.Add(this.lbltrabajo);
+            this.Controls.Add(this.lbltareas);
+            this.Controls.Add(this.lblexamen);
+            this.Controls.Add(this.btnlimpiar);
+            this.Controls.Add(this.btnespanol);
+            this.Controls.Add(this.btningles);
+            this.Controls.Add(this.txttelefono);
+            this.Controls.Add(this.txtapellido);
+            this.Controls.Add(this.txtnombre);
+            this.Controls.Add(this.lbltelefono);
+            this.Controls.Add(this.lblapellido);
+            this.Controls.Add(this.lblnombre);
+            this.Controls.Add(this.txtmatricula);
+            this.Controls.Add(this.lblmatricula);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        internal System.Windows.Forms.TextBox txtresultado;
+        internal System.Windows.Forms.Label lblresultado;
+        internal System.Windows.Forms.Button btncalcular;
+        internal System.Windows.Forms.Label lbl255;
+        internal System.Windows.Forms.Label lbl25;
+        internal System.Windows.Forms.Label lbl50;
+        internal System.Windows.Forms.TextBox txtnota;
+        internal System.Windows.Forms.TextBox txttrabajo;
+        internal System.Windows.Forms.TextBox txttareas;
+        internal System.Windows.Forms.TextBox txtexamen;
+        internal System.Windows.Forms.Label lblnota;
+        internal System.Windows.Forms.Label lbltrabajo;
+        internal System.Windows.Forms.Label lbltareas;
+        internal System.Windows.Forms.Label lblexamen;
+        internal System.Windows.Forms.Button btnlimpiar;
+        internal System.Windows.Forms.Button btnespanol;
+        internal System.Windows.Forms.Button btningles;
+        internal System.Windows.Forms.TextBox txttelefono;
+        internal System.Windows.Forms.TextBox txtapellido;
+        internal System.Windows.Forms.TextBox txtnombre;
+        internal System.Windows.Forms.Label lbltelefono;
+        internal System.Windows.Forms.Label lblapellido;
+        internal System.Windows.Forms.Label lblnombre;
+        internal System.Windows.Forms.TextBox txtmatricula;
+        internal System.Windows.Forms.Label lblmatricula;
     }
 }
 
--- calculo de nota/Form1.cs
+++ calculo de nota/Form1.cs
@@ -16,5 +16,65 @@ namespace calculo_de_nota
         {
             InitializeComponent();
         }
+
+        private void btningles_Click(object sender, EventArgs e)
+        {
+            lblmatricula.Text = "ID";
+            lblnombre.Text = "nombre";
+            lblapellido.Text = "LasT Name";
+            lbltelefono.Text = "phone";
+
+        }
+
+        private void btnespanol_Click(object sender, EventArgs e)
+        {
+            lblmatricula.Text = "ID";
+            lblnombre.Text = "nombre";
+            lblapellido.Text = "apellido";
+            lbltelefono.Text = "telefono";
+
+        }
+
+        private void btnlimpiar_Click(object sender, EventArgs e)
+        {
+            lblmatricula.Text = "";
+            lblnombre.Text = "";
+            lblapellido.Text = "";
+            lbltelefono.Text = "";
+
+        }
+
+        private void btncalcular_Click(object sender, EventArgs e)
+        {
+            
+            double matricula, nombre, apellido, telefono, examen, tareas, trabajo, nota;
+            matricula = Convert.ToDouble(txtmatricula.Text);
+            nombre = Convert.ToDouble(txtnombre.Text);
+            apellido = Convert.ToDouble(txtapellido.Text);
+            telefono = Convert.ToDouble(txttelefono.Text);
+            examen = Convert.ToDouble(txtexamen.Text);
+            tareas = Convert.ToDouble(txttareas.Text);
+            trabajo = Convert.ToDouble(txttrabajo.Text);
+            nota = Convert.ToDouble(txtnota.Text);
+            txtresultado.Text = nota.ToString();
+
+
+            nota = examen + tareas + trabajo;
+            if (Convert.ToDouble(txtnota.Text) >= 70 && Convert.ToDouble(txtnota.Text) <= 100)
+
+            
+
+
+            {
+                MessageBox.Show("Aprobado");
+
+            }
+            else if (Convert.ToDouble(txtnota.Text) <= 69 && Convert.ToDouble(txtnota.Text) <= 69)
+
+            {
+                MessageBox.Show("Reprovado");
+
+            }
+        }
     }
 }
